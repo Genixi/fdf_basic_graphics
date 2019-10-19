9d7d546e93bf11f9ad98582aa5001711087a731c
Eglantine Quiana <equiana@il-e3.21-school.ru>
2019-10-19 17:20:57 +0300

    dd

--- render.c
+++ render.c
@@ -6,7 +6,7 @@
 /*   By: equiana <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 18:03:35 by equiana           #+#    #+#             */
-/*   Updated: 2019/10/18 20:06:37 by equiana          ###   ########.fr       */
+/*   Updated: 2019/10/19 17:20:14 by equiana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,66 @@
 #include <mlx.h>
 #include <stdlib.h>
 #include <stdio.h>
-void ft_bresenham(t_point p0, t_point p1)
+void ft_bresenham(t_param *param, t_point p0, t_point p1)
 {
-	int x0;
-	int 
-
+	t_point	tmp;
+	t_point step;
+	int x;
+	int y;
+	int error;
+	
+	printf("start drawing\n");
+	x = p0.x;
+	y = p0.y;
+	error = 0;
+	step.x = (p0.x < p1.x) ? 1 : -1;
+	step.y = (p0.y < p1.y) ? 1 : -1;
+	//deltax
+	tmp.x = abs(p1.x - p0.x);
+	//deltay
+	tmp.y = abs(p1.y - p0.y);
+	//deltaerr
+	tmp.h = tmp.x - tmp.y;
+	while (x != p1.x || y != p1.y)
+	{
+		mlx_pixel_put(param->mlx_ptr, param->win_ptr, x + WIDTH / 2, (y + HEIGHT / 2) / V_ANGLE, 0x000FFFFFF);
+		if ((error = tmp.h * 2) > -tmp.y)
+		{
+			tmp.h -= tmp.y;
+			x = x + step.x;
+		}
+		if (error < tmp.x)
+		{
+			tmp.y += tmp.x;
+			y = y + step.y;
+		}
+	}
 }
 
 void ft_render(t_list *map)
 {
-	void *mlx_ptr;
-	void *win_ptr;
+	t_param *param;
 	t_list *tmp;
-	int h;
-	int x;
-	int y;
+	t_point p0;
+	t_point p1;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, WIDTH, HEIGHT, "fdf");
-	mlx_key_hook(win_ptr, ft_deal_key, (void *)0); 
+	printf("check reader 0\n");
+	param = NULL;
+	param->mlx_ptr = mlx_init();
+	printf("check render 1\n");
+	param->win_ptr = mlx_new_window(param->mlx_ptr, WIDTH, HEIGHT, "fdf");
+	printf("check render 2\n");
+	mlx_key_hook(param->win_ptr, ft_deal_key, (void *)0); 
 	
+	printf("check reader 3\n");
 	tmp = map;
-	while (tmp)
+	while (tmp && tmp->next)
 	{
-		h = ((t_point*)(tmp->content))->h;
-		x = ((t_point*)(tmp->content))->x;
-		y = ((t_point*)(tmp->content))->y;
-		mlx_pixel_put(mlx_ptr, win_ptr, x + WIDTH / 2,  (y + HEIGHT / 2) / V_ANGLE, 0x000FFFFFF);
+		p1 = *(t_point*)(tmp->next->content);
+		p0 = *(t_point*)(tmp->content);
+		ft_bresenham(param, p0, p1);
 		tmp = tmp->next;
 	}
 	
-	mlx_loop(mlx_ptr);
+	mlx_loop(param->mlx_ptr);
 }
